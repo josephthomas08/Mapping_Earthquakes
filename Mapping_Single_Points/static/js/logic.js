29213a3c00d3873b89a6b9553044695ceeefd50e
josephthomas08 <joseph@tmgcontrol.com>
2021-03-27 12:31:29 -0400

    added modified logic and gitignore file in folder

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,6 +14,18 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+  //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// L.circle([34.0522, -118.2437], {
+//   radius: 100
+// }).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+  color: 'black',
+  fillColor: '#ffffa1',
+  radius: 300
+}).addTo(map);
+
 
 //   // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -28,7 +40,9 @@ let map = L.map("mapid", {
 
 
     // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
